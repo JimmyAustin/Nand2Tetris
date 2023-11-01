d2be778dd93a952c8a59d887bbec246d20a00c69
James Austin <james@repl.it>
2023-10-31 19:49:25 -0700

    Project 4.

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,75 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 //// Replace this comment with your code.
+
+// SCREEN = 16384
+// KBD = 24576
+// Screen Resolution is 256x512 (HxW)
+// Whenever a key is pressed on the physical keyboard, its 16-bit ASCII code appears in RAM[24576].
+
+// 8192 ADDRESSES
+
+// GAME PLAN
+
+// i = 16384
+// (START)
+// RAM[I] = 0
+// JMP TO INCREMENT IF RAM[KBD] == 0
+// RAM[I] = -1
+// (INCREMENT)
+// i += 1
+// JMP TO START if i < 24576
+// i = 16384
+// JMP to START
+
+@SCREEN
+D=A
+@i
+M=D
+@i
+M=D // i = 16384
+(START)
+
+// Jump if keyboard off
+@KBD
+D=M
+@PIXELOFF
+D;JEQ
+
+// Turn pixels ON
+@i
+A=M
+M=-1
+@INCREMENT
+0;JMP
+
+(PIXELOFF)
+// Turn pixels OFF
+@i
+A=M
+M=0
+
+(INCREMENT)
+
+
+// Increment to the next pixel
+@i
+M=M+1
+
+// if i < 24576, JUMP TO 16384
+@i
+D=M
+@24576
+D=D-A
+@START
+D;JLT
+
+// Restart i to start of screen
+@SCREEN
+D=A
+@i
+M=D
+
+// Jump to start
+@START
+0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -8,3 +8,35 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 //// Replace this comment with your code.
+
+// r[2] == 0
+@2
+M=0
+@0
+D=M
+@END
+D;JEQ // Jump to @DEST if ram[0] > 0
+(DEST)
+@1 
+D=M // D = ram[1]
+@2
+M=M+D // ram[2] = ram[2] + D
+@0
+M=M-1 // ram[0] = ram[0] - 1
+D=M // D = ram[0]
+@DEST
+D;JGT // Jump to @DEST if ram[0] > 0
+(END)
+@END
+0;JMP // Infinite loop
+
+// r[2] += r[1]
+// r[0] -= 1
+// IF R[0] > 0; JUMP TO DEST
+// @END
+// 0;JMP
+
+// for (int i = 0; i < R0; i++) {
+//   R2 = R2 + R1   
+// }
+// LOOPFOREVER
